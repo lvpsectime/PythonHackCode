48ca9706868810f04fb16004c8cc60a2c732d462
imysec <1395851238@qq.com>
2019-09-11 15:21:14 +0800

    add 1.1 folder

---
+++ 1.1数值类型/code/1.1.py
@@ -0,0 +1,16 @@
+# _*_ coding:utf-8 _*_
+
+a = 0o101
+print("a="+str(a))
+
+b = 64
+print("b="+str(b))
+
+c = -127
+print("c="+str(c))
+
+d = 0x80
+print("d="+str(d))
+
+e = -0x92
+print("e="+str(e))
